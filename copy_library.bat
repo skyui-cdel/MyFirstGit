bffd54fb6a7a00ba3279e433a27851b1dfb93e02
libo.weng@gmail.com <libo.weng@gmail.com@6d9c6d06-b0b6-dbdc-3531-a27e122abd9f>
2011-04-13 02:33:58 +0000

    去掉third-party ，请在编译之前执行 copy_library.bat

--- copy_library.bat
+++ copy_library.bat
@@ -8,7 +8,7 @@ xcopy "Weibo.common/lib/DLL Debug" "Weibo.sdk/third-party/lib/DLL Debug" /E /Y /
 xcopy "Weibo.common/lib/DLL Release" "Weibo.sdk/third-party/lib/DLL Release" /E /Y /C /I
 
 @rem Include file,if you need,please turn it on.
-::xcopy "Weibo.common/include" "Weibo.sdk/third-party/include" /E /Y /C /I
+xcopy "Weibo.common/include" "Weibo.sdk/third-party/include" /E /Y /C /I
 
 
 pause
